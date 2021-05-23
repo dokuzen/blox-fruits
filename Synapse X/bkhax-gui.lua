b18b7c7d343b02b851c2ba5fa25089de342e210f
Aesu <57193206+dokuzen@users.noreply.github.com>
2021-05-23 18:35:55 +0400

    Working as of UPD 14

--- Synapse X/bkhax-gui.lua
+++ Synapse X/bkhax-gui.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/koonpeatch/PeatEX/master/BKHAX/BloxFruits"),true))() 
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/koonpeatch/PeatEX/master/BKHAX/BloxFruits"),true))()
